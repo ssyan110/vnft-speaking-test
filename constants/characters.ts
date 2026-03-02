0c7466bf86b1c9bcea98c46e8d63ddb98e191d2c
ssyan110 <ssyan110@gmail.com>
2026-03-02 10:37:48 +0700

    Update characters

---
+++ constants/characters.ts
@@ -0,0 +1,148 @@
+import { ChineseCharacter } from '../types';
+
+export const CHINESE_CHARACTERS: ChineseCharacter[] = [
+  {
+    character: '一',
+    pinyin: 'yī',
+    hanViet: 'nhất',
+    meaning: 'một',
+    radical: '一 (yī) – bộ Nhất',
+    characterType: 'Chỉ sự'
+  },
+  {
+    character: '八',
+    pinyin: 'bā',
+    hanViet: 'bát',
+    meaning: 'tám',
+    radical: '八 (bā) – bộ Bát',
+    characterType: 'Chỉ sự'
+  },
+  {
+    character: '五',
+    pinyin: 'wǔ',
+    hanViet: 'ngũ',
+    meaning: 'năm',
+    radical: '二 (èr) – bộ Nhị',
+    characterType: 'Chỉ sự'
+  },
+  {
+    character: '不',
+    pinyin: 'bù',
+    hanViet: 'bất',
+    meaning: 'không',
+    radical: '一 (yī) – bộ Nhất',
+    characterType: 'Giả tá'
+  },
+  {
+    character: '大',
+    pinyin: 'dà',
+    hanViet: 'đại',
+    meaning: 'to, lớn',
+    radical: '大 (dà) – bộ Đại',
+    characterType: 'Tượng hình'
+  },
+  {
+    character: '口',
+    pinyin: 'kǒu',
+    hanViet: 'khẩu',
+    meaning: 'miệng, nhân khẩu',
+    radical: '口 (kǒu) – bộ Khẩu',
+    characterType: 'Tượng hình'
+  },
+  {
+    character: '白',
+    pinyin: 'bái',
+    hanViet: 'bạch',
+    meaning: 'trắng',
+    radical: '白 (bái) – bộ Bạch',
+    characterType: 'Tượng hình'
+  },
+  {
+    character: '女',
+    pinyin: 'nǚ',
+    hanViet: 'nữ',
+    meaning: 'nữ, phụ nữ',
+    radical: '女 (nǚ) – bộ Nữ',
+    characterType: 'Tượng hình'
+  },
+  {
+    character: '你',
+    pinyin: 'nǐ',
+    hanViet: 'nhĩ',
+    meaning: 'bạn, anh, chị',
+    radical: '亻 (rén) – bộ Nhân đứng',
+    characterType: 'Hình thanh'
+  },
+  {
+    character: '好',
+    pinyin: 'hǎo',
+    hanViet: 'hảo',
+    meaning: 'tốt, đẹp',
+    radical: '女 (nǚ) – bộ Nữ',
+    characterType: 'Hội ý'
+  },
+  {
+    character: '马',
+    pinyin: 'mǎ',
+    hanViet: 'mã',
+    meaning: 'ngựa',
+    radical: '马 (mǎ) – bộ Mã',
+    characterType: 'Tượng hình'
+  },
+  {
+    character: '太',
+    pinyin: 'tài',
+    hanViet: 'thái',
+    meaning: 'quá, cực kỳ',
+    radical: '大 (dà) – bộ Đại',
+    characterType: 'Chỉ sự'
+  },
+  {
+    character: '汉',
+    pinyin: 'hàn',
+    hanViet: 'hán',
+    meaning: 'nhà Hán, người Hán',
+    radical: '氵 (shuǐ) – bộ Thủy (ba chấm nước)',
+    characterType: 'Hình thanh'
+  },
+  {
+    character: '语',
+    pinyin: 'yǔ',
+    hanViet: 'ngữ',
+    meaning: 'ngôn ngữ, lời nói',
+    radical: '讠 (yán) – bộ Ngôn',
+    characterType: 'Hình thanh'
+  },
+  {
+    character: '吗',
+    pinyin: 'ma',
+    hanViet: 'ma',
+    meaning: 'trợ từ nghi vấn',
+    radical: '口 (kǒu) – bộ Khẩu',
+    characterType: 'Hình thanh'
+  },
+  {
+    character: '妈',
+    pinyin: 'mā',
+    hanViet: 'ma',
+    meaning: 'mẹ',
+    radical: '女 (nǚ) – bộ Nữ',
+    characterType: 'Hình thanh'
+  },
+  {
+    character: '爸',
+    pinyin: 'bà',
+    hanViet: 'ba',
+    meaning: 'ba, bố',
+    radical: '父 (fù) – bộ Phụ',
+    characterType: 'Hình thanh'
+  },
+  {
+    character: '很',
+    pinyin: 'hěn',
+    hanViet: 'ngận',
+    meaning: 'rất',
+    radical: '彳 (chì) – bộ Sách (bước chân trái)',
+    characterType: 'Hình thanh'
+  }
+];
