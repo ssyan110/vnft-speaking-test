4dceafaf841f80d90d8089156f598a00e836e92b
ssyan110 <ssyan110@gmail.com>
2026-03-16 11:34:34 +0700

    update app experience

--- constants/questions.ts
+++ constants/questions.ts
@@ -3,103 +3,43 @@ import { Question } from '../types';
 
 export const QUESTIONS: Question[] = [
   {
-    pinyin: 'xià tiān qù chī fàn de shí hou, nǐ xǐ huān zuò wài miàn hái shi lǐ miàn, wèi shén me?',
-    chinese: '夏天去吃饭的时候，你喜欢坐外面还是里面，为什么？'
+    pinyin: 'zài yuè nán, kě yǐ qǔ xiāo huǒ chē piào ma? děi fù tuì piào fèi ma?',
+    chinese: '在越南，可以取消火车票吗？得付退票费吗？',
   },
   {
-    pinyin: 'nǐ zuì xǐ huān de cān tīng shì nǎ jiā? qǐng nǐ jiè shào.',
-    chinese: '你最喜欢的餐厅是哪家？请你介绍。'
+    pinyin: 'nǐ xǐ huān zuò tóu děng cāng, shāng wù cāng, hái shi jīng jì cāng? wèi shén me?',
+    chinese: '你喜欢坐头等舱、商务舱、还是经济舱？为什么？',
   },
   {
-    pinyin: 'qù xīn de cān tīng chī fàn, zì jǐ kàn cài dān hé qǐng fú wù yuán tuī jiàn, nǎ ge hǎo? wèi shén me?',
-    chinese: '去新的餐厅吃饭，自己看菜单和请服务员推荐，哪个好？为什么？'
+    pinyin: 'zuò huǒ chē de shí hou, nǐ xǐ huān zuò yìng zuò hái shi ruǎn zuò?',
+    chinese: '坐火车的时候，你喜欢坐硬座还是软座？',
   },
   {
-    pinyin: 'yí gè nián qīng rén, mǎi fáng zi hǎo hái shi zū fáng zi hǎo? wèi shén me?',
-    chinese: '一个年轻人，买房子好还是租房子好？为什么？'
+    pinyin: 'nǐ xǐ huān nǎ jiā háng kōng gōng sī de chéng wù yuán? wèi shén me?',
+    chinese: '你喜欢哪家航空公司的乘务员？为什么？',
   },
   {
-    pinyin: 'shàng wǎng zhǎo fáng zi, yǒu shén me hǎo de, yǒu shén me bù hǎo de?',
-    chinese: '上网找房子，有什么好的，有什么不好的？'
+    pinyin: 'nǐ jué de chéng wù yuán zhè ge gōng zuò zěn me yàng?',
+    chinese: '你觉得乘务员这个工作怎么样？',
   },
   {
-    pinyin: 'nǐ cháng yòng shǒu jī hái shi diàn nǎo shàng wǎng? wèi shén me?',
-    chinese: '你常用手机还是电脑上网？为什么？'
+    pinyin: 'rú guǒ nǐ yào gǎi qiān, nǐ huì zěn me zuò? yòng App, dǎ diàn huà gěi kè fú, hái shi dào guì tái? wèi shén me?',
+    chinese: '如果你要改签，你会怎么做？用App，打电话给客服，还是到柜台？为什么？',
   },
   {
-    pinyin: 'zhù jiǔ diàn de shí hou, měi tiān huàn chuáng dān yǒu shén me hǎo de, yǒu shén me bù hǎo de?',
-    chinese: '住酒店的时候，每天换床单有什么好的，有什么不好的？'
+    pinyin: 'qù wán de shí hou, nǐ cháng zhù duō shǎo qián yǐ nèi de jiǔ diàn?',
+    chinese: '去玩的时候，你常住多少钱以内的酒店？',
   },
   {
-    pinyin: 'nǐ cháng yòng nǎ xiē shè jiāo méi tǐ? nǐ yòng tā zuò shén me?',
-    chinese: '你常用哪些社交媒体？你用它做什么？'
+    pinyin: 'rú guǒ jié hūn, nǐ xiǎng zài jǐ suì yǐ nèi yǒu hái zi?',
+    chinese: '如果结婚，你想在几岁以内有孩子？',
   },
   {
-    pinyin: 'xiǎo hái cháng cháng yòng dǒu yīn, yǒu shén me hǎo de, yǒu shén me bù hǎo de?',
-    chinese: '小孩常常用抖音，有什么好的，有什么不好的？'
+    pinyin: 'nǐ dǎ suàn zài jǐ ge yuè yǐ nèi qù kǎo HSK?',
+    chinese: '你打算在几个月以内去考HSK？',
   },
   {
-    pinyin: 'qǐng nǐ jiè shào nǐ zài gōng sī de yì tiān.',
-    chinese: '请你介绍你在公司的一天。'
+    pinyin: 'nǐ kě yǐ zài duō jiǔ yǐ nèi zuò wán yí fèn bào gào?',
+    chinese: '你可以在多久以内做完一份报告？',
   },
-  {
-    pinyin: 'rú guǒ yào qù tài guó, nǐ huì gēn tuán lǚ yóu hái shi zì yóu xíng, wèi shén me?',
-    chinese: '如果要去泰国，你会跟团旅游还是自由行，为什么？'
-  },
-  {
-    pinyin: 'qù lǚ yóu de shí hou, mǎi jì niàn pǐn yǒu shén me hǎo de? yǒu shén me bù hǎo de?',
-    chinese: '去旅游的时候，买纪念品有什么好的？有什么不好的？'
-  },
-  {
-    pinyin: 'xiàn zài nǐ zuì xiǎng qù nǎ ge guó jiā? wèi shén me?',
-    chinese: '现在你最想去哪个国家？为什么？'
-  },
-  {
-    pinyin: 'nǐ jué de yuè nán zhōng bù lǚ yóu zěn me yàng?',
-    chinese: '你觉得越南中部旅游怎么样？'
-  },
-  {
-    pinyin: 'gēn tuán lǚ yóu yǒu shén me hǎo de, yǒu shén me bù hǎo de?',
-    chinese: '跟团旅游有什么好的，有什么不好的？'
-  },
-  {
-    pinyin: 'zì yóu xíng yǒu shén me hǎo de, yǒu shén me bù hǎo de?',
-    chinese: '自由行有什么好的，有什么不好的？'
-  },
-  {
-    pinyin: 'rú guǒ zhǐ shì xiǎo gǎn mào, nǐ huì qù kàn bìng hái shi mǎi yào? wèi shén me?',
-    chinese: '如果只是小感冒，你会去看病还是买药？为什么？'
-  },
-  {
-    pinyin: 'děi kàn bìng de shí hou, nǐ cháng qù yī yuàn hái shi zhěn suǒ kàn bìng? wèi shén me?',
-    chinese: '得看病的时候，你常去医院还是诊所看病？为什么？'
-  },
-  {
-    pinyin: 'nǐ jué de dà jiā dōu děi zuò jiàn kāng jiǎn chá ma? wèi shén me?',
-    chinese: '你觉得大家都得做健康检查吗？为什么？'
-  },
-  {
-    pinyin: 'nǐ jué de zhǔn shí zhòng yào ma? wèi shén me?',
-    chinese: '你觉得准时重要吗？为什么？'
-  },
-  {
-    pinyin: 'nǐ jué de hé jīng lǐ bào gào gōng zuò jìn dù zhòng yào ma? wèi shén me?',
-    chinese: '你觉得和经理报告工作进度重要吗？为什么？'
-  },
-  {
-    pinyin: 'nǐ yí huí jiā jiù huì zuò shén me?',
-    chinese: '你一回家就会做什么？'
-  },
-  {
-    pinyin: 'nǐ jué de guò nián zuì zhòng yào de shì shén me? wèi shén me?',
-    chinese: '你觉得过年最重要的事是什么？为什么？'
-  },
-  {
-    pinyin: 'guò nián de shí hou, nǐ jiā zuò shén me? chī shén me?',
-    chinese: '过年的时候，你家做什么？吃什么？'
-  },
-  {
-    pinyin: 'guò nián de shí hou, bāo hóng bāo zhòng yào ma? wèi shén me?',
-    chinese: '过年的时候，包红包重要吗？为什么？'
-  }
 ];
